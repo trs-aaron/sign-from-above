36b5f7014267b3bf64fdfa18dfc90743a490d311
Aaron Arterburn <aarterburn@tamarackidaho.com>
2020-12-03 22:09:52 -0700

    Changed vector sign to have SVG as background of element so that it will cover whole area.

--- src/templates/signs/VectorSign.js
+++ src/templates/signs/VectorSign.js
@@ -5,13 +5,14 @@ import * as styles from 'sass/signs/VectorSign.scss';
 const template = (props) => {
     props = {
         isVisible: props.isVisble,
-        vectorPath: (Util.isNonEmptyString(props.vectorPath)) ? props.vectorPath : ''
+        vectorPath: (Util.isNonEmptyString(props.vectorPath)) ? props.vectorPath : '',
+        style: (Util.isNonEmptyString(props.vectorPath)) ? `background-image: url(${props.vectorPath});` : ''
     };
 
     return (
         <div className="tam-sign_vector">
             <div className="tam-sign_vector_cntr">
-                <img className="tam-sign_vector_img" src={props.vectorPath}/> 
+                <div className="tam-sign_vector_img" style={{backgroundImage: "url(" + props.vectorPath + ")"}}/>
             </div>
         </div>
     );
